cd9f2138d22b71622ad96001bae0f696ca0c9f99
Foti.Mougos <foti.mougos@gmail.com>
2020-04-02 21:21:35 +1100

    oops finished everything

--- index.js
+++ index.js
@@ -1,12 +1,99 @@
-const questions = [
+const inquirer = require("inquirer");
+const fs = require('fs');
+const axios = require("axios");
 
-];
+/*
+* At least one badge
+* Project title
+* Description
+* Table of Contents
+* Installation
+* Usage
+* License
+* Contributing
+* Tests
+* Questions
+  * User GitHub profile picture
+  * User GitHub email
+*/
 
-function writeToFile(fileName, data) {
+
+async function queryGitHub(username) {
+    const queryUrl = `https://api.github.com/users/${username}`;
+    this.response;
+    const gitHubResponse = await axios.get(queryUrl);
+    return gitHubResponse;
 }
 
-function init() {
+function writeToFile(answers) {
+    let template = stripIndent`# ${answers.title}
+    Authored by ${answers.username} ( ${answers.email} ) <br>
+    ![Profile Picture]( ${answers.profileImageURL} "Profile Pic")
+    
+    ### Description
+    ${answers.description}
+        
+    ### Table of Contents
+        
+    * Installation
+    * Usage
+    * License
+    * Contributing
+    * Tests
 
+    ### Installation
+    ${answers.install}
+
+    ### Usage
+    ${answers.usage}
+
+    ### License
+    ${answers.licensing}
+
+    ### Contributing
+    ${answers.contribute}
+
+    ### Tests
+    ${answers.testing}
+`
+
+    fs.writeFile("./output/README.md", template, (err) => {
+        return (err) ? ("Error:", err) : "Readme generated";
+    });
+}
+
+async function askQuestions() {
+    const questions = {
+        'username': "Please enter your github username",
+        'email' : "Please enter your email address",
+        'title': "Please enter a project title",
+        'description': "Please enter a project description",
+        'install': "Please enter a comment about installation",
+        'usage': "Please enter a comment about usage",
+        'licensing': "Please enter a comment about licensing",
+        'contribute': "Please enter a comment about contributing to this project",
+        'testing': "Please enter a comment about testing"
+    };
+
+    let answers = {};
+
+    for (var question in questions) {
+        const input = await inquirer.prompt({
+                type: "input",
+                message: questions[question],
+                name: question
+        });
+
+        answers[question] = input[question];
+    }
+    return answers;
+}
+
+async function init() {
+    let answers = await askQuestions();
+    let gitHubData = await queryGitHub(answers.username);
+    answers["profileImageURL"] = gitHubData.data.avatar_url;
+    console.log(writeToFile(answers));
 }
 
 init();
---
+++ output/README.md
@@ -0,0 +1,29 @@
+# The README Generator
+Authored by foteye ( foti.mougos@gmail.com ) <br>
+![Profile Picture]( https://avatars1.githubusercontent.com/u/28522177?v=4 "Profile Pic")
+   
+### Description
+This program allows you to quickly generate a README file by just answering a simple set of questions.
+    
+### Table of Contents
+    
+* Installation
+* Usage
+* License
+* Contributing
+* Tests
+
+### Installation
+You can install this program by downloading this repository and running it standalone
+
+### Usage
+You can just run this in node 12.x. Mileage may vary.
+
+### License
+This is completely open. Do whatever you want with it. I don't care.
+
+### Contributing
+If you legitimately want to pay money for this call 000. You. Need. Help.
+
+### Tests
+HAHAHA
